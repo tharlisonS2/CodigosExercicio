b69e1b33164c54cb41200777b81112725bc07dd2
tharlisonS2 <tharlisonptc2@gmail.com>
2021-08-24 14:57:52 -0300

    Atividade 01

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::resources([
+    "cliente" =>ClienteController::class
+]);
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/v', function () {
+    return view('atividade01');
+});
